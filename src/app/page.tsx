652693ada7ab0e4eaa12c79aba8f05b2b7db08cc
DavidDontKnow <DavidDontKnow02@gmail.com>
2023-07-04 11:11:59 -0700

    Homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,114 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <main>
+      <nav>
+        <h1>Groceries-R-Us</h1>
+        <ul>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/recipes">Recipes</a>
+          </li>
+          <li>
+            <a href="/stores">Stores</a>
+          </li>
+        </ul>
+      </nav>
+
+      <section>
+        <h2>About</h2>
+        <p>
+          Groceries-R-us is a website that helps you find the best recipes, and
+          the best stores to buy the ingredients from.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      </section>
+
+      <section className="p-10">
+        <h2>featured recipe</h2>
+        <div className="featured text-center ">
+          <h3>Chicken Parmesan</h3>
+          <Image
+            src="/chicken-parm.jpg"
+            alt="Chicken Parmesan"
+            width={300}
+            height={300}
+          />
+          <div className="recipe-info">
+            <h3>Ingredients</h3>
+            <ul>
+              <li>4 skinless, boneless chicken breast halves</li>
+              <li>1 cup all purpose flour</li>
+              <li>2 large eggs, beaten</li>
+              <li>1 cup Italian seasoned bread crumbs</li>
+              <li>1/2 cup grated Parmesan cheese</li>
+              <li>2 cups marinara sauce</li>
+              <li>1 cup shredded mozzarella cheese</li>
+              <li>1/4 cup chopped fresh basil</li>
+              <li>1/2 cup olive oil</li>
+              <li>salt and pepper to taste</li>
+            </ul>
+            <h3>Directions</h3>
+            <ol>
+              <li>
+                Preheat oven to 350 degrees F (175 degrees C). Lightly grease a
+                baking sheet.
+              </li>
+              <li>
+                place the chicken breasts between the two sheets of plastic wrap
+                and pound them to an even thickness, about 1/2 inch (1.25 cm)
+                thick
+              </li>
+              <li>Season the chicken breasts with salt and pepper.</li>
+              <li>
+                Set up three shallow bowls or plates. In the first bowl, place
+                the flour. In the second bowl, beat the eggs. In the third bowl,
+                mix together the breadcrumbs and grated Parmesan cheese.
+              </li>
+              <li>
+                Dredge each chicken breast in the flour, shaking off any excess.
+                Then dip it into the beaten eggs, allowing any excess to drip
+                off. Finally, coat the chicken in the breadcrumb-Parmesan
+                mixture, pressing it gently to adhere.
+              </li>
+              <li>
+                Heat a large skillet over medium-high heat and add enough olive
+                oil to coat the bottom of the pan. Place the breaded chicken
+                breasts in the skillet and cook until golden brown on both
+                sides, about 3-4 minutes per side. You may need to cook them in
+                batches depending on the size of your skillet. Remove the cooked
+                chicken breasts and place them on a paper towel-lined plate to
+                drain any excess oil.
+              </li>
+              <li>
+                In a baking dish, spread a thin layer of marinara sauce to cover
+                the bottom. Place the browned chicken breasts on top of the
+                sauce.
+              </li>
+              <li>
+                Spoon additional marinara sauce over each chicken breast,
+                covering them completely.
+              </li>
+              <li>
+                Sprinkle the shredded mozzarella cheese over the chicken
+                breasts.
+              </li>
+              <li>
+                Place the baking dish in the preheated oven and bake for about
+                20-25 minutes, or until the chicken is cooked through and the
+                cheese is melted and bubbly.
+              </li>
+              <li>
+                Remove the chicken Parmesan from the oven and let it rest for a
+                few minutes.
+              </li>
+              <li>Garnish with chopped fresh basil leaves before serving.</li>
+            </ol>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
-  )
+  );
 }
